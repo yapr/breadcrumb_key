dbeb11a229153d65246d5fc3dcb2272b7895f87b
yamitake <take.yapr@gmail.com>
2023-09-27 10:58:22 +0900

    add comment to railtie

--- lib/breadcrumb_key/railtie.rb
+++ lib/breadcrumb_key/railtie.rb
@@ -4,6 +4,8 @@ require "breadcrumb_key/helper"
 require "rails"
 
 module BreadcrumbKey
+  # Railtie integrates BreadcrumbKey::Helper with Rails' ActionView,
+  # based on the `BreadcrumbKey.auto_include_helpers` configuration.
   class Railtie < Rails::Railtie
     initializer "breadcrumb_key.view_helpers" do
       ActiveSupport.on_load :action_view do
